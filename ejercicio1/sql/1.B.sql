47a2d17e2be089da3959df6cb2d4c69d3b73e6ff
moblanoh <91539845+moblanoh@users.noreply.github.com>
2022-05-02 20:32:02 +0200

    Delete ejercicio1 directory

--- ejercicio1/sql/1.B.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Equipos (
-    id INT NOT NULL PRIMARY KEY,
-    id_entreador INT NOT NULL,
-    nombre VARCHAR (20) NOT NULL,
-    division CHAR NOT NULL,
-    ciudad VARCHAR (20) NOT NULL,
-    estado VARCHAR (20) NOT NULL
-);
--- ejercicio1/sql/1.D.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Partidos (
-    id INT NOT NULL PRIMARY KEY,
-    id_equipo_visitante INT NOT NULL,
-    id_equipo_local INT NOT NULL,
-    fecha DATE NOT NULL,
-    goles_local INT NOT NULL,
-    goles_visitante INT NOT NULL
-);
