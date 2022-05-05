73f1a8a3904a8cc999cd69d29160e8df18a3113c
moblanoh <91539845+moblanoh@users.noreply.github.com>
2022-05-05 19:25:05 +0200

    Update tabla_equipos.sql

--- ejercicio1/tabla_equipos.sql
+++ ejercicio1/tabla_equipos.sql
@@ -1,8 +1,10 @@
 CREATE TABLE Equipos (
-    id INT NOT NULL PRIMARY KEY, 
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     id_entreador INT, 
     nombre VARCHAR (20) NOT NULL, 
     division CHAR NOT NULL, 
     ciudad VARCHAR (20) NOT NULL, 
     estado VARCHAR (20) NOT NULL
 );
+
+ALTER TABLE Equipos ADD FOREIGN KEY(id_entreador) REFERENCES Entrenador(id);
