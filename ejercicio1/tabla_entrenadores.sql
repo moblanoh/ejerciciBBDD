61baabdc1ac97b16f09c621014a31c22f14eed0e
moblanoh <91539845+moblanoh@users.noreply.github.com>
2022-05-05 19:24:34 +0200

    Update tabla_entrenadores.sql

--- ejercicio1/tabla_entrenadores.sql
+++ ejercicio1/tabla_entrenadores.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Entrenador (
-    id INT NOT NULL PRIMARY KEY, 
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
     nombre VARCHAR (20) NOT NULL, 
     apellidos VARCHAR (30) NOT NULL, 
     email VARCHAR (30) NOT NULL
