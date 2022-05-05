979239e6be69269b0fdccd8a3ccd4bf8c1b426d4
moblanoh <91539845+moblanoh@users.noreply.github.com>
2022-05-05 19:25:47 +0200

    Update tabla_jugadores.sql

--- ejercicio1/tabla_jugadores.sql
+++ ejercicio1/tabla_jugadores.sql
@@ -1,14 +1,12 @@
 CREATE TABLE Jugadores (
-    id INT NOT NULL PRIMARY KEY, 
-    id_equipo INT , 
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    id_equipo INT, 
     nombre VARCHAR (20) NOT NULL, 
     apellidos VARCHAR (20) NOT NULL, 
     fecha_Nacimiento DATE NOT NULL,
     posicion VARCHAR (20) NOT NULL, 
     sueldo INT NOT NULL,
-    nacionalidad VARCHAR (20) NOT NULL,
+    nacionalidad VARCHAR (20) NOT NULL
 );
 
-ALTER TABLE `Jugador`
-	CHANGE COLUMN `id_equipo` `id_equipo` INT(10) NOT NULL AUTO_INCREMENT AFTER `id`,
-	ADD CONSTRAINT `FK_Jugador_Jugador` FOREIGN KEY (`id_equipo`) REFERENCES `Jugador` (`id_equipo`) ON UPDATE CASCADE ON DELETE SET NULL;
+ALTER TABLE Jugadores ADD FOREIGN KEY(id_equipo) REFERENCES Equipos (id);
