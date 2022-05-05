461ef40685d859d5291ca5cf69c7ca4861500701
moblanoh <91539845+moblanoh@users.noreply.github.com>
2022-05-05 09:24:50 +0200

    Update tabla_partidos.sql

--- ejercicio1/tabla_partidos.sql
+++ ejercicio1/tabla_partidos.sql
@@ -1,7 +1,7 @@
 CREATE TABLE Partidos (
     id INT NOT NULL PRIMARY KEY, 
-    id_equipo_visitante INT NOT NULL, 
-    id_equipo_local INT NOT NULL, 
+    id_equipo_visitante INT, 
+    id_equipo_local INT, 
     fecha DATE NOT NULL, 
     goles_local INT NOT NULL, 
     goles_visitante INT NOT NULL
